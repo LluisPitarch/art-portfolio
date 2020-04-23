171dd039914d4498c52fa4f16f801ebfb7304853
Lluis Pitarch Ripollés <lluispitarchripolles@Lluiss-MacBook-Pro.local>
2020-04-23 19:18:36 +0200

    Updates

---
+++ precache-manifest.0a799455a72964b3ee90158489e335f3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "206ed0972bb3621d6fcaa56280cf0d9a",
+    "url": "/art-portfolio/index.html"
+  },
+  {
+    "revision": "2acbbc03f5e8b6eaa9ee",
+    "url": "/art-portfolio/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "9a8815147b4bcbc1aa01",
+    "url": "/art-portfolio/static/css/main.a68bf7a3.chunk.css"
+  },
+  {
+    "revision": "2acbbc03f5e8b6eaa9ee",
+    "url": "/art-portfolio/static/js/2.db8812f4.chunk.js"
+  },
+  {
+    "revision": "a986d777e7ab357b3f79ca1eede38fb5",
+    "url": "/art-portfolio/static/js/2.db8812f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8815147b4bcbc1aa01",
+    "url": "/art-portfolio/static/js/main.15e41631.chunk.js"
+  },
+  {
+    "revision": "9cdcda432b8ff3f6cbf4",
+    "url": "/art-portfolio/static/js/runtime-main.d7fb168d.js"
+  },
+  {
+    "revision": "54084033c5c2dab86a767191ddda23b6",
+    "url": "/art-portfolio/static/media/artwork-1.54084033.jpg"
+  },
+  {
+    "revision": "8f4662ad4fdca5455477f427f2e109cc",
+    "url": "/art-portfolio/static/media/artwork-2.8f4662ad.jpg"
+  },
+  {
+    "revision": "052969a56a6e6ce01bac238dbf540e25",
+    "url": "/art-portfolio/static/media/artwork-3.052969a5.jpg"
+  },
+  {
+    "revision": "adb41cef4f5531ebcca9d6a5790b38d9",
+    "url": "/art-portfolio/static/media/bio.adb41cef.jpg"
+  },
+  {
+    "revision": "d1e6d963ec4eeb7daedec91aa6d6492a",
+    "url": "/art-portfolio/static/media/home-bg.d1e6d963.jpg"
+  },
+  {
+    "revision": "d5fc955982bbf2426fe941bd742054db",
+    "url": "/art-portfolio/static/media/loader.d5fc9559.gif"
+  },
+  {
+    "revision": "270523e5bafd89f74bfd45453ba948a0",
+    "url": "/art-portfolio/static/media/responsive-home-bg.270523e5.jpg"
+  }
+]);
