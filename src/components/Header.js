a22bb12c20271c490a9578997786e358fed4a0ed
Lluis Pitarch <lluispitarchripolles@gmail.com>
2020-08-31 17:50:28 +0200

    changue the name for the app & add env example flile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,23 @@
 import React from 'react';
 
-import {Link} from 'react-router-dom'
-import MenuRes from './ResponsiveNav'
+import { Link } from 'react-router-dom';
+import MenuRes from './ResponsiveNav';
 
-import './styles/header.css'
-
+import './styles/header.css';
 
 function Header(props) {
-    return (
-        
-        <div className={`${props.gray} header-container`}> 
-            <MenuRes/>
-            <div className="logo">
-            
-                <Link className="logo-nav" to="/">
-                    <h1><span className="light-logo"> Mireia </span>  Ávila</h1>
-                </Link>
-            </div>
-
-        </div>
-        
-    )
+  return (
+    <div className={`${props.gray} header-container`}>
+      <MenuRes />
+      <div className="logo">
+        <Link className="logo-nav" to="/">
+          <h1>
+            <span className="light-logo"> Art </span> Portfolio
+          </h1>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
