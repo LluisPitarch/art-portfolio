86e253dfff969449e5a731f4be8c90824379767f
Lluis Pitarch <lluispitarchripolles@gmail.com>
2020-09-02 19:50:09 +0200

    alt image changes to prevent problems

--- src/components/ArtworkNav.js
+++ src/components/ArtworkNav.js
@@ -1,33 +1,29 @@
 import React from 'react';
 
-import './styles/artworknav.css'
+import './styles/artworknav.css';
 
 class ArtworkNav extends React.Component {
+  render() {
+    const navArtworks = this.props.projects;
 
-    render() { 
-        const navArtworks = this.props.projects
-        
-        return (
-        
-        <ul className="nav-artwork-ul">
-            <div className="rotated-nav">
-
-                <div className="scrolling-div-nav">
-                    {navArtworks.map((navArtworks) => {
-                        return(
-                            <a onClick={this.props.update} href={`/artwork-info/${navArtworks.id}`}>
-
-                                    {navArtworks.title} 
-                            </a>
-                        )
-                    })}
-                </div>
-
-            </div>
-        </ul>
-        )
-    };
+    return (
+      <ul className="nav-artwork-ul">
+        <div className="rotated-nav">
+          <div className="scrolling-div-nav">
+            {navArtworks.map((navArtworks) => {
+              return (
+                <a
+                  onClick={this.props.update}
+                  href={`/artwork-info/${navArtworks.id}`}>
+                  {navArtworks.title}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </ul>
+    );
+  }
 }
- 
-export default ArtworkNav;
 
+export default ArtworkNav;
